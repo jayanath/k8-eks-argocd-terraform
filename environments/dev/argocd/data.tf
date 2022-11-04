2055a896e29f3bb59191497d08383038a115479b
Jayanath Amaranayake <jayanath@gmail.com>
2022-11-04 23:16:41 +1100

    Add the ArgoCD manifests and other resources

---
+++ environments/dev/argocd/data.tf
@@ -0,0 +1,33 @@
+data "terraform_remote_state" "eks" {
+  backend = "s3"
+
+  config = {
+    bucket = "k8-eks-argo-terraform-state-jay"
+    key    = "k8-demo-eks.tfstate"
+    region = "us-west-2"
+  }
+}
+
+data "kubectl_file_documents" "namespace" {
+  content = file("${path.module}/manifests/namespace.yaml")
+}
+
+data "kubectl_file_documents" "argocd" {
+  content = file("${path.module}/manifests/install.yaml")
+}
+
+data "kubectl_file_documents" "grpc" {
+  content = file("${path.module}/manifests/service-grpc.yaml")
+}
+
+data "kubectl_file_documents" "ingress" {
+  content = file("${path.module}/manifests/ingress.yaml")
+}
+
+data "kubectl_file_documents" "repos" {
+  content = file("${path.module}/manifests/app-repos.yaml")
+}
+
+data "kubectl_file_documents" "appset" {
+  content = file("${path.module}/manifests/app-set.yaml")
+}
